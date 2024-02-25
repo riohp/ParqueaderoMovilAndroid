147d323b1446f78b5e5e0a92e38846ca658a5e10
Ramirez-05 <117038114+Ramirez-05@users.noreply.github.com>
2024-02-24 23:20:13 -0500

    lol

---
+++ AppParkingMain/app/src/main/java/com/example/appparking/ui/assingment/AssigmentFragment.java
@@ -0,0 +1,32 @@
+package com.example.appparking.ui.assingment;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.appparking.R;
+import com.example.appparking.databinding.FragmentAssigmentBinding;
+
+
+public class AssigmentFragment extends Fragment {
+
+    private FragmentAssigmentBinding binding;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentAssigmentBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
