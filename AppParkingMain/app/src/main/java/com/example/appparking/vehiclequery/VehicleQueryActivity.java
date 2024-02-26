3635098b457a7654b5dd7e78b92e3b108d80f8d7
EstebanVfx <papaniel2145@gmail.com>
2024-02-25 22:26:55 -0500

    UI Finzalida de consulta servicio

--- AppParkingMain/app/src/main/java/com/example/appparking/vehiclequery/VehicleQueryActivity.java
+++ AppParkingMain/app/src/main/java/com/example/appparking/vehiclequery/VehicleQueryActivity.java
@@ -1,14 +1,43 @@
 package com.example.appparking.vehiclequery;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.appparking.R;
 
 public class VehicleQueryActivity extends AppCompatActivity {
 
+    Button btnViewCar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle_query);
+        btnViewCar = findViewById(R.id.btnViewCar);
+        btnViewCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
     }
+
+    private void showDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheetlayout);
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+    }
+
+
 }
