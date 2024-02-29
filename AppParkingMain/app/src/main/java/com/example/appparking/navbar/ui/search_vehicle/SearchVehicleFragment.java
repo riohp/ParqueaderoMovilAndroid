c121810d35773686353ff33f0979a0f039da0dab
EstebanVfx <papaniel2145@gmail.com>
2024-02-29 11:49:18 -0500

    Vehiculos funcionando lista en vendedor

--- AppParkingMain/app/src/main/java/com/example/appparking/navbar/ui/search_vehicle/SearchVehicleFragment.java
+++ AppParkingMain/app/src/main/java/com/example/appparking/navbar/ui/search_vehicle/SearchVehicleFragment.java
@@ -11,8 +11,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.appparking.R;
 import com.example.appparking.databinding.FragmentSearchVehicleBinding;
+import com.example.appparking.utils.Config;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +32,11 @@ public class SearchVehicleFragment extends Fragment {
 
     EditText campo_busqueda_vehicle;
     RecyclerView recyclerVehiculos;
+
+    Adaptadordevehiculos adaptador;
     private FragmentSearchVehicleBinding binding;
+
+    Config dataConfig;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -30,19 +44,66 @@ public class SearchVehicleFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_search_vehicle, container, false);
 
         recyclerVehiculos = root.findViewById(R.id.recyclerVehiculos);
+        dataConfig = new Config(requireActivity().getApplicationContext());
 
-       List<Vehiculo> listaVehiculos = new ArrayList<>();
-       listaVehiculos.add(new Vehiculo("ABC123", "Moto", "Yamaha", "2021"));
-       listaVehiculos.add(new Vehiculo("ABC123", "Carro", "Chevrolet", "2021"));
-       listaVehiculos.add(new Vehiculo("ABC123", "Carro", "Chevrolet", "2021"));
+       //List<Vehiculo> listaVehiculos = new ArrayList<>();
+       //listaVehiculos.add(new Vehiculo("ABC123", "moto", "Yamaha", "2021"));
+       //listaVehiculos.add(new Vehiculo("ABC123", "carro", "Chevrolet", "2021"));
+       //listaVehiculos.add(new Vehiculo("ABC123", "carro", "Chevrolet", "2021"));
 
        recyclerVehiculos = root.findViewById(R.id.recyclerVehiculos);
        recyclerVehiculos.setLayoutManager(new LinearLayoutManager((getActivity().getApplicationContext())));
-       Adaptadordevehiculos adaptador = new Adaptadordevehiculos(listaVehiculos);
-       recyclerVehiculos.setAdapter(adaptador);
+
+        obtenerVehiculos();
 
         return root;
     }
+
+    private void obtenerVehiculos(){
+        String url = dataConfig.getEndPoint("/getAll/obtenerAll.php?idpark=1");
+
+        StringRequest solicitud = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                List<Vehiculo> listaVehiculos = procesarRespuesta(response);
+                adaptador = new Adaptadordevehiculos(listaVehiculos);
+                recyclerVehiculos.setAdapter(adaptador);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        RequestQueue queue = Volley.newRequestQueue(requireActivity());
+        queue.add(solicitud);
+    }
+
+
+    public List<Vehiculo> procesarRespuesta(String response) {
+        List<Vehiculo> listaVehiculos = new ArrayList<>();
+        try {
+            JSONObject respuesta = new JSONObject(response);
+            JSONArray lista = respuesta.getJSONArray("registros");
+            for (int i = 0; i<lista.length(); i++){
+
+                Vehiculo vehiculo = new Vehiculo(
+                        lista.getJSONObject(i).getString("placa"),
+                        lista.getJSONObject(i).getString("tipovehiculo"),
+                        lista.getJSONObject(i).getString("horaentrada"),
+                        lista.getJSONObject(i).getString("estado")
+                );
+                listaVehiculos.add(vehiculo);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return listaVehiculos;
+    }
+
 
     @Override
     public void onDestroyView() {
